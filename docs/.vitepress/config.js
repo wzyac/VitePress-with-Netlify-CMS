7fbb64f835b87a7c0e9c2af54212df750c9be4c6
wzyac <qq972686389@wzy.ac.cn>
2024-12-03 21:35:54 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: 'VitePress template',
 	description: 'A JAMstack website template with the VitePress and Netlify CMS config.',
 	themeConfig: {
-		repo: 'tsutoringo/VitePress-with-Netlify-CMS',
+		repo: 'wzyac/VitePress-with-Netlify-CMS',
 		docsDir: 'docs',
 		editLinks: true,
 		editLinkText: 'Edit this page on GitHub',
